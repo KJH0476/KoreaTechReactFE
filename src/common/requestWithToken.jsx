6f17e1254be323b0394b1f91e027d27420ad2954
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2024-03-28 09:04:35 +0900

    feat: 공통 메소드 추가
    requestWithToken.js : 요청을 보낼때 토큰값을 넣어 보내도록 함수화 시켜 코드 중복 최소화
    webSocketService.js : 웹소켓 및 stomp 관련 메소드 집합
    decodeToken.js : 토큰 값을 해석해야할때 사용

--- src/common/requestWithToken.jsx
+++ src/common/requestWithToken.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import {actions as userActions} from "../reducers/reducer/userSlice";
-import {actions as friendActions} from "../reducers/reducer/friendSlice";
+import {logout} from "../reducers/reducer/userSlice";
+import {clearFriends} from "../reducers/reducer/friendSlice";
 import {disconnectWebsocket} from "./webSocketService";
 
 export function sendRequestWithToken(apiEndpoint, requestData, method, dispatch) {
     let accessToken = localStorage.getItem('token');
 
-    // Setup axios options
+    // axios options 설정
     let axiosOptions = {
         method: method,
         url: process.env.REACT_APP_SERVER_URL + apiEndpoint,
@@ -22,17 +22,16 @@ export function sendRequestWithToken(apiEndpoint, requestData, method, dispatch)
 
     return axios(axiosOptions)
         .then(async response => {
-            // No need to check for 401 here, axios will throw an error
+            //성공 응답이 오면 그대로 반환
             return { status: response.status, data: response.data };
         })
         .catch(async error => {
             console.log(error.response.data);
             if (error.response.data && error.response.status === 401) {
                 const errorMessage = error.response.data.message;
-                //만료된 토큰 삭제
-                //window.localStorage.removeItem('token');
                 console.log(errorMessage);
-                //새로운 토큰을 발급받았을 경우에 다시 로컬 스토리지에 토큰 저장후 재요청
+
+                // 새로운 토큰을 발급받았을 경우에 다시 로컬 스토리지에 토큰 저장후 재요청
                 if(errorMessage==='NEW_ACCESS_TOKEN') {
                     const newToken = error.response.data.token;
                     //새로운 액세스 토큰으로 교체
@@ -45,14 +44,15 @@ export function sendRequestWithToken(apiEndpoint, requestData, method, dispatch)
                         .then(res => ({ status: res.status, data: res.data }))
                         .catch(error => {throw new Error('Refresh token expired')});
                 }
+                // 리프레시 토큰도 만료되었을 경우 로그아웃
                 else {
                     console.log("리프레시 토큰도 만료, 로그아웃처리");
                     disconnectWebsocket(dispatch);
-                    await dispatch(userActions.logout()); // 로그아웃 액션 디스패치
-                    await dispatch(friendActions.clearFriends()); // 친구 목록 초기화
+                    window.localStorage.removeItem('token');
+                    await dispatch(logout()); // 로그아웃 액션 디스패치
+                    await dispatch(clearFriends()); // 친구 목록 초기화
                 }
             } else {
-                // Handle other errors
                 throw error;
             }
         });
